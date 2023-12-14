7af48a50e510726025f98ca1b42fd1e39922dd97
Armando Del Rio <adelriorz@gmail.com>
2023-12-13 22:52:49 -0700

    Made .env work

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,6 +1,6 @@
-import environ
-import os 
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,18 +9,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# Add .env and load variables anywhere before SECRET_KEY
+load_dotenv(find_dotenv())
+
 # SECURITY WARNING: keep the secret key used in production secret!
-
-env = environ.Env(
-  # set casting, default value
-  DEBUG=(bool, False)
-)
-
-SECRET_KEY = env('SECRET_KEY')
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel/app', '.now.sh', '127.0.0.1', 'localhost']
 
